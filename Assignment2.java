0f5b6ba414fff4ea139f954f0ba10656287a977f
Tom Brady <35128051+MingoLink@users.noreply.github.com>
2018-04-17 16:03:32 -0700

    Add files via upload

--- Assignment2.java
+++ Assignment2.java
@@ -10,9 +10,8 @@ public class Assignment2 {
 
 	public LinkedList<Integer> partition(List<Integer> list, int E) {
 		
-		// create two LinkedList to be sorted into LinkedLists above and below E
-		LinkedList<Integer> belowE = new LinkedList<Integer>();
-		LinkedList<Integer> aboveE = new LinkedList<Integer>();
+		// create LinkedList to be sorted into LinkedLists above and below E
+		LinkedList<Integer> sorted = new LinkedList<Integer>();
 
 		// Create iterator
 		Iterator<Integer> itr = list.iterator();
@@ -22,21 +21,15 @@ public class Assignment2 {
 			
 			int i = itr.next();
 			if (i < E) {
-				belowE.add(i); // sort numbers below E
+				sorted.addFirst(i); // sort numbers below E. Add to beginning of set
 
 			} else if (i > E) {
-				aboveE.add(i); // sort numbers above E
+				sorted.addLast(i); // sort numbers above E. Add to end of set
 			}
 		}
-		
-		// create LinkedList to combine numbers below and above E
-		LinkedList<Integer> combined = new LinkedList<Integer>();
-		combined.addAll(belowE);
-		combined.addAll(aboveE);
 
-		
 		// return LinkedList with combined results
-		return combined;
+		return sorted;
 
 	}
 
