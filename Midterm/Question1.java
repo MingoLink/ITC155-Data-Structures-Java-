5369bde000de0b3c48ff8a6ca07b607f4abd7a5e
Tom Brady <35128051+MingoLink@users.noreply.github.com>
2018-05-15 15:59:48 -0700

    Update Question1.java

--- Midterm/Question1.java
+++ Midterm/Question1.java
@@ -1,30 +1,30 @@
 public int maxOccurences(List<Integer> list) {
 		
-		Map<Integer,Integer> max = new HashMap<Integer,Integer>();
+	Map<Integer,Integer> max = new HashMap<Integer,Integer>();
 		
-		//iterate through list
-		for(int i : list) {
+	//iterate through list
+	for(int i : list) {
     
-			//see if integer is already in list
-			if(max.containsKey(i)) {
+		//see if integer is already in list
+		if(max.containsKey(i)) {
       
-				//set count to previous integer occurrence
-				int count = max.get(i);
-				max.put(i, count + 1);
+			//set count to previous integer occurrence
+			int count = max.get(i);
+			max.put(i, count + 1);
         
-			}else {
-				//if its not in there place it in map
-				max.put(i, 1);
-			}
+		}else {
+			//if its not in there place it in map
+			max.put(i, 1);
 		}
-		
-		// check for largest int and store in maxInt value
-		int maxInt = 0;
-		for(int j : max.values()) {
-			if (j > maxInt) {
-				maxInt = j;
-			}
-		}
-		
-		return maxInt;
 	}
+	
+		
+	// check for largest int and store in maxInt value
+	int maxInt = 0;
+	for(int j : max.values()) {
+		if (j > maxInt) {
+			maxInt = j;
+		}
+	}	
+	return maxInt;
+}
